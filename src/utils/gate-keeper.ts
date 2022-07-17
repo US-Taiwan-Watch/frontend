ea369b52667d8350e70fad1816a87701284ed13e
jingwan <hsugaigai@gmail.com>
2022-07-17 16:34:26 -0700

    Fix gatekeeper

--- src/utils/gate-keeper.ts
+++ src/utils/gate-keeper.ts
@@ -1,4 +1,5 @@
-const showProdContent = false;
+// This should be false while you are delveloping some new feature.
+// Set it to true if you are working on something to be released.
+const hideDev = false;
 
-export const showDevContent = process.env.NODE_ENV && !showProdContent;
-
+export const showDevContent = process.env.NODE_ENV === 'development' && !hideDev;
