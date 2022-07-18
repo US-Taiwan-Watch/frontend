8e790a59cbde2f2edba895ca89dd6abb8f45409e
jingwan <hsugaigai@gmail.com>
2022-07-17 17:13:39 -0700

    Update URLs

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,15 +2,15 @@ export const Constants = {
   links: {
     email: 'contact@ustw.watch',
     fb: 'https://www.facebook.com/ustaiwanwatch',
-    ig: 'https://www.instagram.com/ustaiwanwatch/',
+    ig: 'https://www.instagram.com/ustaiwanwatch',
     yt: 'https://www.youtube.com/USTaiwanWatch',
     twitter: 'https://twitter.com/ustaiwanwatch',
     podcast: 'https://open.spotify.com/show/5CnwG4Tfr7YaQ42FETAI5t',
     // podcast: 'https://podcasts.apple.com/us/podcast/%E7%BE%8E%E5%9C%8B%E5%8F%B0%E7%81%A3%E8%A7%80%E6%B8%AC%E7%AB%99/id1508245836',
-    volunteer: 'https://volunteer.ustw.watch/',
-    donate: 'https://donate.ustw.watch/',
-    donateTW: 'https://donatetw.ustw.watch/',
-    newsletter: 'https://subscribe.ustw.watch/',
+    volunteer: 'https://volunteer.ustw.watch',
+    donate: 'https://donate.ustw.watch',
+    donateTW: 'http://donatetw.ustw.watch',
+    newsletter: 'https://subscribe.ustw.watch',
   },
   partners: [
     {
