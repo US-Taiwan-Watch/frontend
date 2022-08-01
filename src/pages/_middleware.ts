02d66b2f6b0232dc42917a31a1d195f364b47948
jingwan <hsugaigai@gmail.com>
2022-07-31 17:07:03 -0700

    i18n: take the quality of accepted lang and preferred lang into consideration.
    1. If a user explicitly set the language, set that to preferred one in the cookie
    2. The order: preferred lang and accepted lang ordered by quality. If all not supported, by default uses en

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { parse } from "accept-language-parser"
 
 const PUBLIC_FILE = /\.(.*)$/;
 
@@ -9,8 +10,21 @@ export function middleware(request: NextRequest) {
     request.nextUrl.locale === "default";
 
   if (shouldHandleLocale) {
+    let langs = [];
+    if ('preferred-language' in request.cookies) {
+      langs.push(request.cookies['preferred-language']);
+    }
     const acceptLang = request.headers.get("accept-language");
-    const lang = acceptLang?.startsWith("zh") ? "zh-TW" : "en";
-    return NextResponse.redirect(`/${lang}${request.nextUrl.pathname}`);
+    if (acceptLang) {
+      langs = [...langs, ...parse(acceptLang).map(lang => lang.code)];
+    }
+    let finalLang = 'en'; // default one
+    for (const lang of langs) {
+      if (['zh', 'en'].includes(lang)) {
+        finalLang = lang;
+        break;
+      }
+    }
+    return NextResponse.redirect(`/${finalLang}${request.nextUrl.pathname}`);
   }
 }
